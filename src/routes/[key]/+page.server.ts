2a45db90f379245060ac6fa8a849fbf5a4deb495
Soham Sen <contact@sohamsen.me>
2023-06-09 01:11:21 +0530

    For E2E, decrypt and render client side

--- src/routes/[key]/+page.server.ts
+++ src/routes/[key]/+page.server.ts
@@ -8,7 +8,7 @@ import sanitize from 'sanitize-html';
 import sodium from 'libsodium-wrappers';
 
 /** @type {PageServerLoad} */
-export async function load({ params, url }) {
+export async function load({ params }) {
 	const { key } = params;
 
 	const data = await prisma.paste.findUnique({
@@ -18,27 +18,11 @@ export async function load({ params, url }) {
 	if (!data) throw error(404, 'Not found');
 
 	let { content, language, encrypted } = data;
-	const nonceKeyB64 = url.searchParams.get('k');
-
-	if (encrypted && nonceKeyB64) {
-		await sodium.ready;
-		try {
-			if (!nonceKeyB64) throw new Error('Missing key');
-			const [nonceB64, keyB64] = decodeURIComponent(nonceKeyB64).split(';');
-			const nonce = sodium.from_base64(nonceB64);
-			const key = sodium.from_base64(keyB64);
-			const decrypted = sodium.crypto_secretbox_open_easy(sodium.from_base64(content), nonce, key);
-			content = sodium.to_string(decrypted);
-		} catch (e: any) {
-			console.log(e);
-			throw error(400, 'Invalid key');
-		}
-	}
 
 	let contentHtml: string;
 
 	try {
-		if (language !== 'plaintext') {
+		if (!encrypted && language !== 'plaintext') {
 			loadLanguages([language]);
 			contentHtml = Prism.highlight(content, Prism.languages[language], language);
 		} else {
@@ -51,6 +35,8 @@ export async function load({ params, url }) {
 
 	return {
 		content,
-		contentHtml
+		contentHtml,
+		encrypted,
+		language
 	};
 }
--- src/routes/[key]/+page.svelte
+++ src/routes/[key]/+page.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
+	import { AutoLoader } from 'svelte-prism-autoloader';
 	import type { PageData } from './$types';
 
+	let Prism: any;
+
 	export let data: PageData;
+	let { content, contentHtml, language, encrypted } = data;
+	let isSafe = true;
+	let isDecrypted = false;
+	let codeRef: HTMLElement;
+
+	$: if (isDecrypted && codeRef) {
+		(async () => {
+			const Prism = (await import('prismjs')).default;
+			Prism.highlightElement(codeRef);
+		})();
+	}
 
 	let cmdKey = 'Ctrl';
-
 	onMount(() => {
 		const isMac =
 			(navigator as any).userAgentData?.platform?.toLowerCase() === 'macos' ||
@@ -29,16 +43,46 @@
 				copyContent();
 			}
 		});
+
+		if (encrypted) {
+			contentHtml = 'Decrypting...';
+			(async () => {
+				const _sodium = (await import('libsodium-wrappers')).default;
+
+				try {
+					await _sodium.ready;
+					const sodium = _sodium;
+
+					const nonceKeyB64 = $page.url.searchParams.get('k');
+					if (!nonceKeyB64) throw new Error('Missing key');
+					const [nonceB64, keyB64] = decodeURIComponent(nonceKeyB64).split(';');
+					const nonce = sodium.from_base64(nonceB64);
+					const key = sodium.from_base64(keyB64);
+					const decrypted = sodium.crypto_secretbox_open_easy(
+						sodium.from_base64(content),
+						nonce,
+						key
+					);
+					content = sodium.to_string(decrypted);
+
+					isSafe = false;
+					isDecrypted = true;
+				} catch (e) {
+					console.error(e);
+					contentHtml = 'Failed to decrypt';
+				}
+			})();
+		}
 	});
 
 	function copyContent() {
-		navigator.clipboard.writeText(data.content);
+		navigator.clipboard.writeText(content);
 	}
 </script>
 
 <div class="pb-4">
 	<div class="flex flex-row items-center gap-4">
-		<h1 class="mr-auto text-2xl">YAbin</h1>
+		<h1 class="mr-auto text-2xl"><a href="/">YAbin</a></h1>
 
 		<button
 			class="btn underline underline-offset-4 px-2 py-1"
@@ -51,7 +95,7 @@
 		<button
 			class="btn underline underline-offset-4 px-2 py-1"
 			title="{cmdKey}+N"
-			on:click={() => goto('/new')}
+			on:click={() => goto('/')}
 		>
 			New
 		</button>
@@ -66,8 +110,13 @@
 	</div>
 </div>
 
-<div class="whitespace-pre bg-dark p-4 line-numbers">
-	{@html data.contentHtml}
+<div class="whitespace-pre bg-dark p-4 overflow-x-scroll">
+	{#if isSafe}
+		{@html contentHtml}
+	{:else}
+		<pre><code bind:this={codeRef} class="language-{language}">{content}</code></pre>
+		<AutoLoader languagesPath="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/" />
+	{/if}
 </div>
 
 <svelte:head>
@@ -79,3 +128,11 @@
 		referrerpolicy="no-referrer"
 	/>
 </svelte:head>
+
+<style>
+	pre {
+		background-color: var(--color-dark) !important;
+		padding: 0 !important;
+		margin: 0 !important;
+	}
+</style>
